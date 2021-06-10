b55b403e112436b7e8f4a639b27d2fb2dde84721
Victor Ivan Diaz Dzib <lordbounced97@gmail.com>
2021-06-10 15:44:12 -0700

    added amazon url to allowed hosts

--- sitec/sitec/settings.py
+++ sitec/sitec/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^w)lf%byosvqct#z(el%yxb+(kb^uc@l-yfsj%*iv35r2i)n6p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['victordiaz18.pythonanywhere.com']
+ALLOWED_HOSTS = ['ec2-18-234-103-204.compute-1.amazonaws.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- sitec/static/js/login.js
+++ sitec/static/js/login.js
@@ -1,7 +1,6 @@
 $(window).on('load', function(){
 
     let loginForm = $('.login.form').form({
-
     }).api({
         on: 'submit',
         serializeForm: true,
@@ -11,14 +10,17 @@ $(window).on('load', function(){
         beforeSend: function(settings){
             return settings
         },
-        onSuccess: function(){
+        onSuccess: function(response){
             window.location.href = '/sitec/panel/'
         },
         onError: function(){
+            console.log("asdasd")
             $('body').toast({
                 message: 'Hubo un error al iniciar sesion.',
                 class: 'error'
             })
+        },
+        onComplete: function(response, element, xhr){
         }
     })
 })
